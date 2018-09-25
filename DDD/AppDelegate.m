3ddf088a18f23fae9b34e6d09a024a2ba2aaf1ac
denglb3 <libing.deng@midea.com>
2018-09-25 10:24:03 +0800

    添加大文件
    11
    
    33

--- DDD/AppDelegate.m
+++ DDD/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "AvoidCrash.h"
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+//    [AvoidCrash makeAllEffective];
+//    [AvoidCrash setupNoneSelClassStringsArr:@[@"NSString",@"NSArray",@"NSNumber",@"NSDictionary",@"NSPlaceholderString"]];
+
     return YES;
 }
 
